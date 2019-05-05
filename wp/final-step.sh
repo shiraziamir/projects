6a69b4b07b1db43dc4a8730e1c67494000bba06b
Your Name <you@example.com>
2019-05-05 12:44:52 +0430

    update wp script

--- wp/final-step.sh
+++ wp/final-step.sh
@@ -1,2 +1,10 @@
+#docker-compose down -v
+#docker-compose up
 cd /var/lib/docker/volumes/wp_wordpress/_data
-mv * blog/
+mkdir .blog
+mv * .blog
+mv .blog blog
+chown -R --reference=blog/index.php .
+
+# cd /opt/docker/wp
+# docker-compse up -d
